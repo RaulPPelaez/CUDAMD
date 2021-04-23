c42918fa926d0d16421ba70ddf0875c4e8bb3b19
RaulPPealez <raul.perez@uam.es>
2021-04-23 12:26:52 +0200

    Integrator: Add some comments

--- src/Integrator/Integrator.cuh
+++ src/Integrator/Integrator.cuh
@@ -70,9 +70,10 @@ namespace uammd{
       sys->log<System::MESSAGE>("[Integrator] %s created.", name.c_str());
       sys->log<System::MESSAGE>("[Integrator] Acting on group %s", pg->getName().c_str());
     }
-
+    //Take the simulation to the next time step
     virtual void forwardTime() = 0;
 
+    //Add to each particle (via ParticleData::getEnergy) the energy due to the Integrator (typically the kinetic energy)
     virtual real sumEnergy(){ return 0.0;}
 
     //Add an Interactor to the Integrator.
@@ -97,8 +98,7 @@ namespace uammd{
       return {updatables.begin(), updatables.end()};
     }
 
-};
-
+  };
 }
 
 #endif
