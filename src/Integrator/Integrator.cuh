4f7f4736bbd92d3eed13c5a9d99331ae86f123c3
RaulPPealez <raul.perez@uam.es>
2021-04-23 17:14:02 +0200

    Use the updatables member in all available Integrators

--- src/Integrator/Integrator.cuh
+++ src/Integrator/Integrator.cuh
@@ -84,6 +84,7 @@ namespace uammd{
       this->addUpdatable(an_interactor);
     }
 
+    //Get a list of the Interactors that have been added to the Integrator
     std::vector<std::shared_ptr<Interactor>> getInteractors(){
       return interactors;
     }
@@ -94,6 +95,8 @@ namespace uammd{
       updatables.emplace(an_updatable);
     }
 
+    //Get a list of the ParameterUpdatables that have been added to the Integrator
+    //Note that this also includes the Interactors
     std::vector<std::shared_ptr<ParameterUpdatable>> getUpdatables(){
       return {updatables.begin(), updatables.end()};
     }
